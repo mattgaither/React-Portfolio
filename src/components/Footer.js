e4bca1c9e2480cdaa26ec3137a24b1e47a7b5f90
mattgaither <benjamin.gaither@shipt.com>
2022-08-28 07:24:58 -0500

    updated the footer page with icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,23 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 
 const Footer = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <a href="https://www.linkedin.com/in/benjamin-gaither-88732794/">LinkedIn</a>
-        </li>
-        <li>
-          <a href="https://github.com/mattgaither">GitHub</a>
-        </li>
-      </ul>
+    <div className="main-footer">
+      <div className="icon-container">
+        <a href="https://github.com/mattgaither">
+          <FaGithub className="github-icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/benjamin-gaither-88732794/"
+          className="linkedin"
+        >
+          <FaLinkedin className="linkedin-icon" />
+        </a>
+        <a href="mailto:mattgaither2@gmail.com">
+          <FaEnvelope className="email-icon" />
+        </a>
+      </div>
     </div>
   );
 };
